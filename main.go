ef0c93763883dd34f714ebd38e271ab6cd277e92
Snorre Søvold <73354016+snorresovold@users.noreply.github.com>
2023-03-07 12:12:01 +0100

    Update main.go

--- main.go
+++ main.go
@@ -7,19 +7,18 @@ import (
 )
 
 func main() {
-	c, err := ftp.Dial("ftp://snorresovold%40gmail.com@pockethost.ioftp.example.org:21", ftp.DialWithTimeout(5*time.Second))
+	c, err := ftp.Dial("snorresovold%40gmail.com@pockethost.io", ftp.DialWithTimeout(5*time.Second))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = c.Login("anonymous", "anonymous")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-err = c.Login("anonymous", "anonymous")
-if err != nil {
-    log.Fatal(err)
-}
+	// Do something with the FTP conn
 
-// Do something with the FTP conn
-
-if err := c.Quit(); err != nil {
-    log.Fatal(err)
-}
+	if err := c.Quit(); err != nil {
+		log.Fatal(err)
+	}
 }
