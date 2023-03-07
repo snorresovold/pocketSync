02c808c42cc3514aa25fd0ed3d42151bfa91915b
Snorre Søvold <73354016+snorresovold@users.noreply.github.com>
2023-03-07 12:15:06 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,24 +1,2 @@
-package main
-
-import (
-	"log"
-	"github.com/jlaffaye/ftp"
-	"time"
-)
-
-func main() {
-	c, err := ftp.Dial("snorresovold%40gmail.com@pockethost.io", ftp.DialWithTimeout(5*time.Second))
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = c.Login("anonymous", "anonymous")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Do something with the FTP conn
-
-	if err := c.Quit(); err != nil {
-		log.Fatal(err)
-	}
-}
+//https://pocketbase.io/docs/api-collections/
+// DO THIS!!!
