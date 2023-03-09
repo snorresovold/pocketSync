675a67a206242d88211ce3ae9dd4d22d89b81ed9
snorresovold <snorresovold@gmail.com>
2023-03-09 18:53:06 +0100

    working on console

--- index.ts
+++ index.ts
@@ -1,31 +1,31 @@
-// get the schema to outport
-import PocketBase from 'pocketbase';
+#!/usr/bin/env node
+import pocketSync from "./dist/schemaSync"
+import { Command } from "commander"
 
-/**
- * This function will synchronise the schema of the "in" instance with the "out" instance
- * @param in_email, the email of the instance you are getting the schema from
- * @param out_email, the email of the instance you are outporting to
- * @param in_pswd, the password of the instance you are getting the schema from, MAKE SURE TO KEEP THIS SECRET
- * @param out_pswd, the password of the instance you are outporting to, MAKE SURE TO KEEP THIS SECRET
- * @param in_url, the url of the instance you are getting the schema from
- * @param out_url, the url of the instance you are outporting to
- */
+/*
 
-interface pocketSync{
-    in_email: string
-    out_email: string
-    in_pswd: string
-    out_pswd: string
-    in_url: string
-    out_url: string
+
+ /$$$$$$$                     /$$                   /$$      /$$$$$$                               
+| $$__  $$                   | $$                  | $$     /$$__  $$                              
+| $$  \ $$ /$$$$$$   /$$$$$$$| $$   /$$  /$$$$$$  /$$$$$$  | $$  \__/ /$$   /$$ /$$$$$$$   /$$$$$$$
+| $$$$$$$//$$__  $$ /$$_____/| $$  /$$/ /$$__  $$|_  $$_/  |  $$$$$$ | $$  | $$| $$__  $$ /$$_____/
+| $$____/| $$  \ $$| $$      | $$$$$$/ | $$$$$$$$  | $$     \____  $$| $$  | $$| $$  \ $$| $$      
+| $$     | $$  | $$| $$      | $$_  $$ | $$_____/  | $$ /$$ /$$  \ $$| $$  | $$| $$  | $$| $$      
+| $$     |  $$$$$$/|  $$$$$$$| $$ \  $$|  $$$$$$$  |  $$$$/|  $$$$$$/|  $$$$$$$| $$  | $$|  $$$$$$$
+|__/      \______/  \_______/|__/  \__/ \_______/   \___/   \______/  \____  $$|__/  |__/ \_______/
+                                                                      /$$  | $$                    
+                                                                     |  $$$$$$/                    
+                                                                      \______/                     
+*/
+
+const program = new Command();
+program.description('Our New CLI');
+program.version('0.0.1');
+
+async function main() {
+    await program.parseAsync();
+
 }
-async function pocketSync({in_email, out_email, in_pswd, out_pswd, in_url, out_url} :pocketSync) {
-    const im = new PocketBase('https://scarce-salesmen.pockethost.io');
-    await im.admins.authWithPassword('snorresovold@gmail.com', 'SuzukiFraSverige05!');
-    let im_collections = await im.collections.getFullList();
+console.log() // log a new line so there is a nice space
+main();
 
-    const out = new PocketBase('http://127.0.0.1:8090');
-    await out.admins.authWithPassword('snorresovold@gmail.com', 'ToyotaFraBiltema05!');
-
-    await out.collections.import(im_collections, true);
-}
